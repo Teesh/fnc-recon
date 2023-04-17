b9ed7b444c79cf1e5ffb95befe280b34a22d84bd
Teesh <teesh.s93@gmail.com>
2023-04-17 16:06:59 -0400

    Add SQL connector

--- index.ts
+++ index.ts
@@ -1,15 +1,17 @@
-import express, { Express, Request, Response } from 'express';
-import dotenv from 'dotenv';
+import express, { Express, Request, Response } from 'express'
+import dotenv from 'dotenv'
+import { createUnixSocketPool } from './sqlconnector'
 
-dotenv.config();
+dotenv.config()
 
-const app: Express = express();
-const port = process.env.PORT;
+const app: Express = express()
+const port = process.env.PORT
+// const sql = createUnixSocketPool({})
 
 app.get('/', (req: Request, res: Response) => {
-  res.send('Welcome to the FNC Recon Tool');
-});
+  res.send('Welcome to the FNC Recon Tool')
+})
 
 app.listen(port, () => {
-  console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
-});
+  console.log(`⚡️[server]: Server is running at http://localhost:${port}`)
+})
